3393586bbe7418fa64f2867b43ad0775097e79e1
TJ Draper <tj@caddis.co>
2015-10-23 18:12:08 -0500

    Add recursive directory support for partials

--- system/user/addons/template_sync/Service/PartialFileTemplates.php
+++ system/user/addons/template_sync/Service/PartialFileTemplates.php
@@ -4,6 +4,8 @@ namespace BuzzingPixel\Addons\TemplateSync\Service;
 
 class PartialFileTemplates
 {
+	private $returnTemplates = array();
+
 	/**
 	 * Get partial file templates from file system
 	 *
@@ -11,27 +13,40 @@ class PartialFileTemplates
 	 */
 	public function get($path)
 	{
-		// Load helpers
-		$dirArray = ee('template_sync:DirectoryArrayHelper');
-
 		// Set variables
 		$templateBasePath = SYSPATH . 'user/templates/';
 		$path = $templateBasePath . ee()->config->item('site_short_name') .
 			'/' . $path . '/';
-		$returnFileTemplates = array();
 
 		// Make sure directory exists
 		if (! is_dir($path)) {
 			mkdir($path);
 		}
 
-		// Get templates from file system
+		// Recursively load files
+		$this->processRecursively($path);
+
+		return $this->returnTemplates;
+	}
+
+	/**
+	 * Process directories recursively
+	 *
+	 * @param string $path
+	 * @param string $prefix
+	 */
+	private function processRecursively($path, $prefix = '')
+	{
+		// Load helpers
+		$dirArray = ee('template_sync:DirectoryArrayHelper');
+
 		$templates = $dirArray->process($path);
 
-		// Get template names and contents
 		foreach ($templates as $template) {
-			// Make sure we only include files
+			// Process sub-directories
 			if (is_dir($path . $template)) {
+				$this->processRecursively($path . $template . '/', $prefix . $template . ':');
+
 				continue;
 			}
 
@@ -40,10 +55,8 @@ class PartialFileTemplates
 
 			// Make sure hidden files (starting with .) are not included
 			if ($pathInfo['filename']) {
-				$returnFileTemplates[$pathInfo['filename']] = $content;
+				$this->returnTemplates[$prefix . $pathInfo['filename']] = $content;
 			}
 		}
-
-		return $returnFileTemplates;
 	}
 }
